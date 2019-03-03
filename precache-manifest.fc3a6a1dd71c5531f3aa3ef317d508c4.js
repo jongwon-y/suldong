19d813ce7962612be29c2afdd21646e299ebe9ec
jongwony <yjw963@gmail.com>
2019-03-03 17:03:25 +0900

    Updates

---
+++ precache-manifest.fc3a6a1dd71c5531f3aa3ef317d508c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6db8153cba36741832d4",
+    "url": "/suldong/static/js/runtime~main.6db8153c.js"
+  },
+  {
+    "revision": "ffaf84fa5bc8829951dc",
+    "url": "/suldong/static/js/main.ffaf84fa.chunk.js"
+  },
+  {
+    "revision": "d9d70c7cf35721592991",
+    "url": "/suldong/static/js/2.d9d70c7c.chunk.js"
+  },
+  {
+    "revision": "ffaf84fa5bc8829951dc",
+    "url": "/suldong/static/css/main.495ecd02.chunk.css"
+  },
+  {
+    "revision": "33695130b41aee363b42af9d8713935a",
+    "url": "/suldong/index.html"
+  }
+];
